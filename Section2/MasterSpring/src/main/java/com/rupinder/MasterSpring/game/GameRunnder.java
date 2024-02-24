5395f301cc71cc3be8b9a1f48a68a35c9d17c654
Rupinder <rss264264@gmail.com>
2024-02-24 22:10:35 +0530

    @section-2 @part5 @iteration1 - tight coupled java code

--- Section2/MasterSpring/src/main/java/com/rupinder/MasterSpring/game/GameRunnder.java
+++ Section2/MasterSpring/src/main/java/com/rupinder/MasterSpring/game/GameRunnder.java
@@ -8,5 +8,9 @@ public class GameRunnder {
 
     public void run() {
         System.out.println("Running game: "+game);
+        game.up();
+        game.down();
+        game.left();
+        game.right();
     }
 }
--- Section2/MasterSpring/src/main/java/com/rupinder/MasterSpring/game/MarioGame.java
+++ Section2/MasterSpring/src/main/java/com/rupinder/MasterSpring/game/MarioGame.java
@@ -1,4 +1,17 @@
 package com.rupinder.MasterSpring.game;
 
 public class MarioGame {
+    public void up(){
+        System.out.println("jump");
+    }
+    public void down(){
+        System.out.println("got into a hole");
+    }
+    public void left(){
+        System.out.println("stop");
+    }
+
+    public void right(){
+        System.out.println("Accelarate");
+    }
 }
